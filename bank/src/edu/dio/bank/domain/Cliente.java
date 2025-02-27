fc719deda3182a664a96d8922dbe7580bb3b302d
Emanuel <123523520+emanueldias01@users.noreply.github.com>
2025-02-27 09:22:13 -0300

    criacao de classes conta e cliente;

---
+++ bank/src/edu/dio/bank/domain/Cliente.java
@@ -0,0 +1,9 @@
+package edu.dio.bank.domain;
+
+import java.time.LocalDate;
+
+public class Cliente {
+    private String nome;
+    private String cpf;
+    private LocalDate dataNascimento;
+}
---
+++ bank/src/edu/dio/bank/domain/Conta.java
@@ -0,0 +1,9 @@
+package edu.dio.bank.domain;
+
+public class Conta {
+    private final int numeroAgencia = 1;
+    private int id;
+
+    private Double saldo;
+    private Cliente cliente;
+}
